4287fed32af699b9287bb027762318ae7086ef5c
Akapi895 <83953516+Akapi895@users.noreply.github.com>
2024-04-23 23:41:36 +0700

    commit

--- CAVESTORY/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "defs.h"
-#include "graphics.h"
-#include "game.h"
-
-int main(int argc, char* argv[])
-{
-    Game game;
-    return 0;
-}
--- UNICAVE/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#include <SDL.h>
-
-int main() {
-	std::cout << 123 << '\n';
-}
